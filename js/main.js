a230aabe7e175c87425ef0076dd32e9d6cc945c7
cheezily <pm655@yahoo.com>
2016-04-21 20:27:18 -0500

    added local storage ability

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-var recipies = [{'name': 'PB&J', 'ingredients': ['Bread', 'Jam', 'Peanut Butter']}]
+//localStorage.removeItem('_cheezily_recipes');
 
-var Recipies = React.createClass({
+if (localStorage['_cheezily_recipes']) {
+  console.log('local storage used');
+  var recipes = JSON.parse(localStorage.getItem('_cheezily_recipes'));
+} else {
+  console.log('STORAGE SET');
+  var recipes = [
+    {"name": "PB&J", "ingredients": ["Bread", "Jam", "Peanut Butter"]},
+    {"name": "Salad", "ingredients": ["Spinach", "Croutons", "Cheese", "More Cheese"]},
+    {"name": "Cheeseburger", "ingredients": ["Bun", "Burger Meat", "Cheese", "More Cheese", "Lettuce", "Mayo"]}
+  ];
+  localStorage.setItem('_cheezily_recipes', JSON.stringify(recipes));
+}
+
+var firstRecipie = recipes[0];
+console.log('first ' + firstRecipie['name']);
+
+var Dishes = React.createClass({
+  setInitialState: function() {
+    return {}
+  },
   render: function() {
-    {this.props.dishes.map(function(dish) {
-      <div>{dish.name}</div>
-    })}
+    return (
+      <div>
+      {this.props.dishes.map(function(dish, key) {
+        return <div className='dish' key={key}>{dish.name}</div>
+      })}
+      </div>
+    )
+
   }
 });
 
@@ -14,21 +38,18 @@ var IngredientList = React.createClass({
   },
   render: function() {
 
-    return (<div>
-      <ul>
-      {this.props.dish.map(function(ingredient, key) {
-        return <div key={key}>
-        <h3 key={key}>{ingredient.name}</h3>
-        {ingredient.ingredients.map(function(item, key) {
-          return <li key={key}>{item}</li>
-        })}
-        </div>
-      })}
-      </ul>
+    return (
+      <div className='ingredientList'>
+          <h3>{this.props.dish.name}</h3>
+          {this.props.dish.ingredients.map(function(item, key) {
+            return <li key={key}>{item}</li>
+          })}
+
       </div>)
-  }
-});
+    }
+  });
 
 
 
-ReactDOM.render(<IngredientList dish={recipies}/>, document.getElementById('output'));
+ReactDOM.render(<IngredientList dish={firstRecipie}/>, document.getElementById('ingredients'));
+ReactDOM.render(<Dishes dishes={recipes}/>, document.getElementById('dishes'));
